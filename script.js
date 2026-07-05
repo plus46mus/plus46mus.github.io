a912ad33136be345b390afec088d94f25fdc2e10
Raz "Vruzzen" Skarnaes <skarnas.rasmus@gmail.com>
2026-07-05 14:31:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ setTimeout(() => {
   p.innerText = "Here is a collection of all my music.";
   document.body.appendChild(p);
 }, 2000);
-
-setTimeout(() => {
-  const p = document.createElement("p");
-  p.innerText = "If you are looking to commission or hire me,";
-  document.body.appendChild(p);
-}, 2000);
